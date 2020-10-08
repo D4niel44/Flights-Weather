86eef08c0049e30fa6247ba6d182181741365627
daniel44 <daniel.linares@ciencias.unam.mx>
2020-10-08 13:56:46 -0500

    refactor type declarations to begining

--- src/proyecto1/openweathermap/openweathermap.go
+++ src/proyecto1/openweathermap/openweathermap.go
@@ -10,6 +10,48 @@ import (
 	g "myp.ciencias.unam.mx/geo"
 )
 
+// Units is an enum for temperature units in an Api response.
+type Units string
+
+const (
+	// METRIC for temperature values in Celsius degrees.
+	METRIC Units = "metric"
+	// IMPERIAL for temperature values in Fahrenheit degrees.
+	IMPERIAL Units = "imperial"
+	// STANDARD for temperature values in Kelvin degrees.
+	STANDARD Units = "standard"
+)
+
+// Language is an enum for supported response languages when querying the API.
+// For now only requests in english, spanish and french are supported.
+type Language string
+
+const (
+	// EN for query responses in english.
+	EN Language = "en"
+	// ES for query responses in spanish.
+	ES Language = "es"
+	// FR for query response in french.
+	FR Language = "fr"
+)
+
+// Weather represents a json response from OpenWeatherAPI.
+// For simplicity only some fields of the OpenWeatherMap reponse are provided.
+type Weather struct {
+	Weather []struct {
+		ID          int    `json:"id"`
+		Main        string `json:"name"`
+		Description string `json:"description"`
+	} `json:"weather"`
+	Main struct {
+		Temp      float32 `json:"temp"`
+		FeelsLike float32 `json:"feels_like"`
+		TempMin   float32 `json:"temp_min"`
+		TempMax   float32 `json:"temp_max"`
+		Humidity  float32 `json:"humidity"`
+	} `json:"main"`
+}
+
 // API represents an OpenWatherApi client to make climate and weather requests.
 type API struct {
 	hash string
@@ -72,48 +114,6 @@ func makeQuery(url string) (*Weather, error) {
 		return nil, jsonErr
 	}
 	return weather, nil
-}
-
-// Units is an enum for temperature units in an Api response.
-type Units string
-
-const (
-	// METRIC for temperature values in Celsius degrees.
-	METRIC Units = "metric"
-	// IMPERIAL for temperature values in Fahrenheit degrees.
-	IMPERIAL Units = "imperial"
-	// STANDARD for temperature values in Kelvin degrees.
-	STANDARD Units = "standard"
-)
-
-// Language is an enum for supported response languages when querying the API.
-// For now only requests in english, spanish and french are supported.
-type Language string
-
-const (
-	// EN for query responses in english.
-	EN Language = "en"
-	// ES for query responses in spanish.
-	ES Language = "es"
-	// FR for query response in french.
-	FR Language = "fr"
-)
-
-// Weather represents a json response from OpenWeatherAPI.
-// For simplicity only some fields of the OpenWeatherMap reponse are provided.
-type Weather struct {
-	Weather []struct {
-		ID          int    `json:"id"`
-		Main        string `json:"name"`
-		Description string `json:"description"`
-	} `json:"weather"`
-	Main struct {
-		Temp      float32 `json:"temp"`
-		FeelsLike float32 `json:"feels_like"`
-		TempMin   float32 `json:"temp_min"`
-		TempMax   float32 `json:"temp_max"`
-		Humidity  float32 `json:"humidity"`
-	} `json:"main"`
 }
 
 type getRequestURLBuilder struct {
