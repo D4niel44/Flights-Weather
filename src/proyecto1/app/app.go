fe4776d1460242ebe278df4f6f90fb8a9e5e1f14
daniel44 <daniel.linares@ciencias.unam.mx>
2020-10-08 13:48:45 -0500

    app with bugs

--- src/proyecto1/app/app.go
+++ src/proyecto1/app/app.go
@@ -2,25 +2,51 @@ package main
 
 // TODO Investigate separating module app from main.
 import (
+	"encoding/csv"
+	"errors"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
+	g "myp.ciencias.unam.mx/geo"
 	owm "myp.ciencias.unam.mx/proyecto1/openweathermap"
+	owmCities "myp.ciencias.unam.mx/proyecto1/openweathermapcities"
 )
 
-// Runs the app.
-func main() {
-	panic("not implemented") // TODO
+// Flight is a struct representing a flight
+type Flight struct {
+	origin      *City
+	destination *City
+}
+
+// City is a struct representing a city
+type City struct {
+	name       string
+	coordinate *g.Coordinate
+	weather    *owm.Weather
+}
+
+var defaultCity = City{
+	name:       "Mexico City",
+	coordinate: &g.Coordinate{19.4363, -99.072098},
 }
 
 // App struct that represents an App for querying cities weather.
 type App struct {
 	API *owm.API
+	DB  *owmCities.OwmCityConverter
 }
 
 // NewApp Creates A New App
-func NewApp(apiKey string) *App {
-	return &App{API: owm.NewAPI(apiKey)}
+func NewApp(apiKey, dbPath string) *App {
+	db, _ := owmCities.NewOwmCityConverter(dbPath)
+	return &App{API: owm.NewAPI(apiKey), DB: db}
+}
+
+func (app *App) Close() {
+	app.DB.Close()
 }
 
 // TODO Improve documentation.
@@ -39,25 +65,115 @@ func NewApp(apiKey string) *App {
 // origin_longitude|longitud_origen origin city longitude coordinate
 // destination_latitude|latitud_destino destination city latitude coordinate
 // destination_longitude|longitud_destino destination city longitude coordinate
-// This method will try to return the names in a OpenWeatherMap call ready string.
-func (app *App) HandleDataSet(dataset string) (*[]*Flight, *map[string]*City) {
-	panic("not implemented") // TODO
+// This method will try to make all its cities ready to call owm API by coordinates
+func (app *App) HandleDataSet(dataset string) (*[]*Flight, map[string]*City) {
+	fileReader, err := os.Open(dataset)
+	if err != nil {
+		panic("error reading dataset") // TODO dont stop because of this
+	}
+	defer fileReader.Close()
+
+	// read headers
+	csvReader := csv.NewReader(fileReader)
+	headersRow, err := csvReader.Read()
+	if err != nil {
+		panic("error reading csv")
+	}
+
+	// index to fields we want
+	var index *Index
+	if headersRow[0] == "destino" { // We are in dataset 1
+		index = &Index{-1, 0, -1, -1, -1, -1}
+	} else {
+		index = &Index{0, 1, 2, 3, 4, 5}
+	}
+
+	var flights []*Flight
+	cities := make(map[string]*City)
+	if index.origin == -1 || index.destination == -1 {
+		cities[owmCities.ToAlphaNumeric(defaultCity.name)] = &defaultCity
+	}
+	rows, err := csvReader.ReadAll()
+	if err != nil {
+		panic("error while reading csv")
+	}
+	fc := FlightCreator{db: app.DB, index: index, flights: flights, cities: cities}
+	for _, row := range rows {
+		fc.addFlight(row)
+	}
+	return &fc.flights, cities
+}
+
+func (fc *FlightCreator) addFlight(row []string) {
+	if fc.index.origin == -1 && fc.index.destination == -1 {
+		fmt.Print("Not enough information about flight.")
+		return
+	}
+	newFlight := Flight{}
+	origin := fc.createCity(row, fc.index.origin, fc.index.originLat, fc.index.originLon)
+	origin = addToCities(fc.cities, origin)
+	newFlight.origin = origin
+	destination := fc.createCity(row, fc.index.destination, fc.index.destinationLat, fc.index.detinationLon)
+	destination = addToCities(fc.cities, destination)
+	newFlight.destination = destination
+	resultingFlights := append(fc.flights, &newFlight)
+	fc.flights = resultingFlights
+}
+
+func (fc *FlightCreator) createCity(row []string, cityI, latI, lonI int) *City {
+	var city City
+	if cityI == -1 {
+		city = defaultCity
+	} else {
+		cityName := strings.TrimSpace(row[cityI])
+		city = City{name: cityName}
+		coordinate, err := fc.db.GetCityCoordinates(cityName)
+		if err != nil && latI != -1 && lonI != -1 {
+			lat, _ := strconv.ParseFloat(row[latI], 64)
+			lon, _ := strconv.ParseFloat(row[lonI], 64)
+			coordinate, _ = g.NewCoordinate(lat, lon)
+		}
+		city.coordinate = coordinate
+	}
+	return &city
+}
+
+func addToCities(cities map[string]*City, city *City) *City {
+	normalizedCityName := owmCities.ToAlphaNumeric(city.name)
+	previousCity, found := cities[normalizedCityName]
+	if found {
+		if previousCity.coordinate == nil && city.coordinate != nil {
+			previousCity.coordinate = city.coordinate
+		}
+		return previousCity
+	}
+	cities[normalizedCityName] = city
+	return city
 }
 
 // QueryWeather Requests the weather for all cities in the map and saves the result in
 // the wethaer field of city.
 //
 // Requires the weather field of city to be nil.
-func (app *App) QueryWeather(cities *map[string]*City) {
+func (app *App) QueryWeather(cities map[string]*City) {
 	queriesCounter := 0
-	for cityName, city := range *cities {
+	for _, city := range cities {
 		if queriesCounter > 55 {
 			time.Sleep(time.Minute)
 			queriesCounter = 0
 		}
-		cityWeather, err := app.API.GetWeatherFromCity(cityName, owm.METRIC, owm.ES)
+		var cityWeather *owm.Weather
+		var err error
+		if city.coordinate != nil {
+			cityWeather, err = app.API.GetWeatherFromCoordinates(city.coordinate, owm.METRIC, owm.ES)
+		} else if city.name != "" {
+			cityWeather, err = app.API.GetWeatherFromCity(city.name, owm.METRIC, owm.ES)
+		} else {
+			err = errors.New("insuficcient data about city")
+		}
 		if err != nil {
-			fmt.Printf("ERROR: %v, \n", err) // TODO Rework this using logger
+			// FIXME strange value bug.
+			fmt.Printf("ERROR: %v, \n %v %s", err, city.coordinate, city.name) // TODO Rework this using logger
 		} else {
 			city.weather = cityWeather
 		}
@@ -68,25 +184,40 @@ func (app *App) QueryWeather(cities *map[string]*City) {
 // TODO improve documentation.
 
 // PrintWeather Prints the weather to standard output
-func PrintWeather(*[]*Flight) {
-	panic("not implemented") // TODO
+func PrintWeather(flights *[]*Flight) {
+	for _, flight := range *flights {
+		fmt.Print("####################### \n")
+		printCityWeather("Origen", flight.origin)
+		fmt.Print("----------------------- \n")
+		printCityWeather("Destino", flight.destination)
+	}
 }
 
-// Flight is a struct representing a flight
-type Flight struct {
-	origin      *City
-	destination *City
+// TODO change this to class method?
+
+func printCityWeather(header string, city *City) {
+	fmt.Printf("%s: %s \n", header, city.name)
+	if city.weather == nil {
+		fmt.Print("Clima no Encontrado")
+	} else {
+		fmt.Printf("Descripcion: %s \n", city.weather.Weather[0].Description)
+		fmt.Printf("Temperatura Mínima: %.1f °C\n", city.weather.Main.TempMin)
+		fmt.Printf("Temperatura Máxima: %.1f °C\n", city.weather.Main.TempMax)
+		fmt.Printf("Humedad: %.1f %% \n", city.weather.Main.Humidity)
+	}
 }
 
-// City is a struct representing a city
-type City struct {
-	name        *string
-	coordinates Coordinates
-	weather     *owm.Weather
+type FlightCreator struct {
+	db      *owmCities.OwmCityConverter
+	index   *Index
+	flights []*Flight
+	cities  map[string]*City
 }
-
-// Coordinates is a struct representing coordinates
-type Coordinates struct {
-	lat float32
-	lon float64
+type Index struct {
+	origin         int
+	destination    int
+	originLat      int
+	originLon      int
+	destinationLat int
+	detinationLon  int
 }
--- src/proyecto1/app/app_test.go
+++ src/proyecto1/app/app_test.go
@@ -2,28 +2,47 @@ package main
 
 import (
 	"testing"
+
+	g "myp.ciencias.unam.mx/geo"
 )
 
 const (
-	testAPIKey = "fc249a737bd3772566de957539505054"
+	testAPIKey      = "fc249a737bd3772566de957539505054"
+	testDBPath      = "../../../bin/db/cities.db"
+	testDatasetPath = "../../../datasets/"
 )
+
+var app = NewApp(testAPIKey, testDBPath)
+
+func TestDataset1(t *testing.T) {
+	testDataset("dataset1.csv", t)
+}
+
+func TestDataset2(t *testing.T) {
+	testDataset("dataset2.csv", t)
+}
+
+func testDataset(dataset string, t *testing.T) {
+	flights, cities := app.HandleDataSet(testDatasetPath + dataset)
+	app.QueryWeather(cities)
+	PrintWeather(flights)
+}
 
 // TODO Improve tests to check no ban when more than 60 cities are requested.
 // TestWueryTest runs simple test to check QueryTest is working
 func TestQuerySet(t *testing.T) {
 	cityName := "Mexico City"
 	city := City{
-		name: &cityName,
-		coordinates: Coordinates{
-			lat: 19,
-			lon: -99,
+		name: cityName,
+		coordinate: &g.Coordinate{
+			Lat: 19,
+			Lon: -99,
 		},
 	}
-	app := NewApp(testAPIKey)
 	cities := make(map[string]*City)
 	cities[cityName] = &city
 	t.Run("Simple Test", func(t *testing.T) {
-		app.QueryWeather(&cities)
+		app.QueryWeather(cities)
 		if cities[cityName].weather == nil {
 			t.Error("Expected non empty weather")
 		}
