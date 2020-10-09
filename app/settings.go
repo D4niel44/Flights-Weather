f98047da5dd0b4ed78f8a394cde202c43e04fcb2
daniel44 <daniel.linares@ciencias.unam.mx>
2020-10-08 22:35:52 -0500

    good settings

--- app/settings.go
+++ app/settings.go
@@ -1,7 +1,7 @@
 package main
 
 const (
-	API_KEY       = "f5945a52edc33c862b71cb618575e8a"
+	API_KEY       = "f5945a52edc33c862b71cb618575e8a7"
 	DATASETS_PATH = "datasets/"
 	DB_PATH       = "db/cities.db"
 )
